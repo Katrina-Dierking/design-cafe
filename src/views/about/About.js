c73e8e61937e7a05b1d86b9c83c40c1a03586c27
Katrina <kalynndier@gmail.com>
2023-05-14 08:09:10 -0400

    about

---
+++ src/views/about/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../components/layout/Layout'
+
+const About = () => {
+  return (
+   <Layout title="ABOUT">
+   
+    </Layout>
+  )
+}
+
+export default About
