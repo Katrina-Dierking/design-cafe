33a8331ba8f15fa33424c8da6763f0112377d322
Katrina <kalynndier@gmail.com>
2023-05-24 10:32:57 -0400

    blog.data added

---
+++ src/views/blog/data.js
@@ -0,0 +1,18 @@
+export const blogs=[
+    {
+        id: 1, 
+        category:"lovemugs", 
+        title: "wizard of oz",
+        body: {
+            p1: "Do you ever feel like you're stuck in some dream (or nightmare) searching for a way back to something familiar? A way to a better place where things make sense? A place where hope abides and the world doesn't seem quite so bleak?", 
+            intro: "Hi, I'm Katrina Dierking -- founder of Queen Bean Design Cafe and the creater of Love Mugs. I had the idea to create these Love Mugs and place them in various places across the country to be relocated to another spot by whomever finds them. The goal is to spread hope to whomever may find them.", 
+            h2One: "But Why a Wizard of Oz Love Mug?", 
+            p2: "When I think of the Wizard of Oz, I am reminded of the unlikely friends Dorothy ran into along her journey home. She needed these friends. She couldn't have found her way home without them. They had her back. They encouraged her and confided in her. They carried her, wiped her tears, and walked beside her every step of the way.",
+            p3: "but what if there had been no friends? What if she had found herself in a strange place and had to navigate the new territory on her own? We've all been there. We've all found ourself in a situation that had us in turmoil. A situation that twisted our insides and had us doubting ourselves. Our future. Our very existance and identity. We were lost with no way out. ", 
+            h2Two: "How Love Mugs Offer Hope", 
+            p4:"As I was creating the Wizard of Oz Love Mug, I was actually dealing with a bit of depression myself. I've entered into a new era of my life and it's uncomfortable. It's uncertain. I've found myself fearful and even at times, hopeless."
+
+        },
+
+    }
+]
