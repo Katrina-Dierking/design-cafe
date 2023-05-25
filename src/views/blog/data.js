d28152aefde577887d19e9ceedcc1d4c01d2c654
Katrina <kalynndier@gmail.com>
2023-05-25 14:39:47 -0400

    blog2

--- src/views/blog/data.js
+++ src/views/blog/data.js
@@ -5,7 +5,8 @@ export const blogs=[
         title: "wizard of oz",
         body: [
             
-            {p1: "Do you ever feel like you're stuck in some dream (or nightmare) searching for a way back to something familiar? A way to a better place where things make sense? A place where hope abides and the world doesn't seem quite so bleak?"
+            {
+            p1: "Do you ever feel like you're stuck in some dream (or nightmare) searching for a way back to something familiar? A way to a better place where things make sense? A place where hope abides and the world doesn't seem quite so bleak?"
             }, 
             {
             intro: "Hi, I'm Katrina Dierking -- founder of Queen Bean Design Cafe and the creater of Love Mugs. I had the idea to create these Love Mugs and place them in various places across the country to be relocated to another spot by whomever finds them. The goal is to spread hope to whomever may find them."
@@ -24,12 +25,21 @@ export const blogs=[
             p4:"As I was creating the Wizard of Oz Love Mug, I was actually dealing with a bit of depression myself. I've entered into a new era of my life and it's uncomfortable. It's uncertain. I've found myself fearful and even at times, hopeless."
             },
             {
-            br: ""
-            },
-            {
             p5: "But as I set out to sculpt the Wizard of Oz Love Mug, I remembered the quote from the movie: 'A heart is shown not how much you love, but how much you are loved by others.' No matter what happens, I know I'm loved. I know my life has value. If the house falls, when the storm comes, when I lose my way -- I am still loved. I am never alone. Even when I feel alone."
             },
         ],
-
-    }
+    }, 
+    {
+        id: 2, 
+        category:"lovemugs", 
+        title: "freedom fighters",
+        body: [
+            {
+                p1: "Maybe you're like me. You have a loved one serving in the Armed Forces. Maybe that brings you great pride but also a bit of fear. You are not alone."
+            },
+            {
+                intro: "Hi, I'm Katrina -- founder of Queen Bean Design Cafe and the creation of Love Mugs, an attempt to bring a bit of hope to the ones who find these Love Mugs along the way."
+            },
+        ],
+    },
 ]
