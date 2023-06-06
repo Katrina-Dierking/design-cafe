df4afaf52f22f21eb3088f13b2c3e6c2604c62cf
Katrina <kalynndier@gmail.com>
2023-06-06 14:17:03 -0400

    font added

--- src/views/shop/shopViews/ShopHome.js
+++ src/views/shop/shopViews/ShopHome.js
@@ -12,26 +12,26 @@ const ShopHome = () => {
     <nav className='shopNav'>
         <div className='shopLeft'>
             <Link to="/sweetshop">sweet shop</Link>
-            <Link to="/tumblers">tumblers</Link>
-            <Link to="/pens">dip pens</Link>
-            <Link to="/pins"> pins | pushpins</Link>
-            <Link to="/lovemugs">love mugs</Link>
-            <Link to="/vinyls">vinyls</Link>
-            <Link to="/straws">collapsible straws</Link>
-        </div>
-        <div className='shopRight'>cart</div>
-    </nav>
-    <main>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente similique quidem expedita porro beatae nesciunt nemo praesentium, cum minima omnis minus voluptates adipisci pariatur dicta architecto totam ex consequuntur perferendis!
-    <br></br>
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non sint et neque corrupti quaerat eum consectetur nemo autem consequuntur libero illo id temporibus, repellat exercitationem. Architecto, voluptatibus. Eaque, ipsa corrupti?
-    <br></br>
-    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non excepturi placeat dolores beatae officia, maiores enim dolor voluptas quas! Eligendi modi dolores doloribus reiciendis neque facilis animi! Sunt, dolorum eligendi.
-    </main>
-      <Footer />
-    
-    </section>
-  
-  )
-}
-
-export default ShopHome
+            </div>
+            <div className='shopRight'>cart</div>
+            </nav>
+            <main>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente similique quidem expedita porro beatae nesciunt nemo praesentium, cum minima omnis minus voluptates adipisci pariatur dicta architecto totam ex consequuntur perferendis!
+            <br></br>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non sint et neque corrupti quaerat eum consectetur nemo autem consequuntur libero illo id temporibus, repellat exercitationem. Architecto, voluptatibus. Eaque, ipsa corrupti?
+            <br></br>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non excepturi placeat dolores beatae officia, maiores enim dolor voluptas quas! Eligendi modi dolores doloribus reiciendis neque facilis animi! Sunt, dolorum eligendi.
+            </main>
+            <Footer />
+            
+            </section>
+            
+            )
+          }
+          
+          export default ShopHome
+          // <Link to="/tumblers">tumblers</Link>
+          // <Link to="/pens">dip pens</Link>
+          // <Link to="/pins"> pins | pushpins</Link>
+          // <Link to="/lovemugs">love mugs</Link>
+          // <Link to="/vinyls">vinyls</Link>
+          // <Link to="/straws">collapsible straws</Link>
