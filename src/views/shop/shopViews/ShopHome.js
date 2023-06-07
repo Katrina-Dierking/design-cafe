32327b03e26d4d7b7d0047a39eba8e1910ab91c4
Katrina <kalynndier@gmail.com>
2023-06-06 22:46:02 -0400

    shop title

--- src/views/shop/shopViews/ShopHome.js
+++ src/views/shop/shopViews/ShopHome.js
@@ -6,34 +6,56 @@ import creative from '../../../assets/images/creative-design.png'
 import cupcake from '../../../assets/images/cupcake.png'
 import soap from '../../../assets/images/soap.png'
 import gift from '../../../assets/images/gift.png'
+import coffee from '../../../assets/images/coffee-beans.png'
 
 const ShopHome = () => {
 
 
   return (
-    <section className='shop'>
-    <h2>~ Queen Bean Shop ~</h2>
-    <nav className='shopNav'>
-        <div className='shopLeft'>
-            <Link to="/sweetshop"><img src={cupcake} className='shopIcon' alt='the sweet tooth' ></img></Link>
-            <img src={soap} className='shopIcon' alt='The sweet and salty soap'/>            <img src={creative} className='shopIcon' alt='The sweet and salty soap box' />
-            <img src={gift} className='shopIcon' alt='The sweet and salty soap box' />
-            
-        
-            </div>
-            <div className='shopRight'>cart</div>
-            </nav>
-            <main>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente similique quidem expedita porro beatae nesciunt nemo praesentium, cum minima omnis minus voluptates adipisci pariatur dicta architecto totam ex consequuntur perferendis!
-            <br></br>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non sint et neque corrupti quaerat eum consectetur nemo autem consequuntur libero illo id temporibus, repellat exercitationem. Architecto, voluptatibus. Eaque, ipsa corrupti?
-            <br></br>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non excepturi placeat dolores beatae officia, maiores enim dolor voluptas quas! Eligendi modi dolores doloribus reiciendis neque facilis animi! Sunt, dolorum eligendi.
-            </main>
-            <Footer />
-            
-            </section>
-            
-            )
+    <section className="shop">
+      <div className='titleBox'>
+        <img src={coffee} className="coffee" alt="coffee bean" />
+        <h2>Queen Bean Shop</h2>
+        <img src={coffee} className="coffee" alt="coffee bean" />
+      </div>
+      <nav className="shopNav">
+        <div className="shopLeft">
+          <Link to="/sweetshop">
+            <img src={cupcake} className="shopIcon" alt="the sweet tooth"></img>
+          </Link>
+          <img src={soap} className="shopIcon" alt="The sweet and salty soap" />{" "}
+          <img
+            src={creative}
+            className="shopIcon"
+            alt="The sweet and salty soap box"
+          />
+          <img
+            src={gift}
+            className="shopIcon"
+            alt="The sweet and salty soap box"
+          />
+        </div>
+        <div className="shopRight">cart</div>
+      </nav>
+      <main>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+        similique quidem expedita porro beatae nesciunt nemo praesentium, cum
+        minima omnis minus voluptates adipisci pariatur dicta architecto totam
+        ex consequuntur perferendis!
+        <br></br>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non sint et
+        neque corrupti quaerat eum consectetur nemo autem consequuntur libero
+        illo id temporibus, repellat exercitationem. Architecto, voluptatibus.
+        Eaque, ipsa corrupti?
+        <br></br>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non excepturi
+        placeat dolores beatae officia, maiores enim dolor voluptas quas!
+        Eligendi modi dolores doloribus reiciendis neque facilis animi! Sunt,
+        dolorum eligendi.
+      </main>
+      <Footer />
+    </section>
+  );
           }
           
           export default ShopHome
